eccf1b7a3d62ce5948b627c539fcfc89166c00c9
victorwu2004 <weiWu@us.mufg.jp>
2025-09-06 18:23:17 -0400

    add tf config

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    # Example: AWS provider
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the provider (example for AWS)
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Add a resource (example: an S3 bucket)
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name"
+}
