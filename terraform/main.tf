ff0d6924a7de6d1452b248aa6b7f671d70476ebe
victorwu2004 <weiWu@us.mufg.jp>
2025-09-13 17:16:17 -0400

    try complete example

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,8 +41,30 @@ resource "aws_iam_role" "github_actions_role" {
 }
 
 # 4. Attach a policy to the IAM role
-resource "aws_iam_role_policy" "github_actions_policy" {
-  name = "GitHubActionsPolicy"
+# resource "aws_iam_role_policy" "github_actions_policy" {
+  # name = "GitHubActionsPolicy"
+  # role = aws_iam_role.github_actions_role.id
+
+  # policy = jsonencode({
+    # Version = "2012-10-17",
+    # Statement = [
+      # {
+        # Effect = "Allow",
+        # Action = [
+          # "s3:ListBucket",
+          # "s3:GetObject",
+          # "s3:PutObject"
+        # ],
+        # Resource = [
+          # "arn:aws:s3:::terraform-S3bucket-test",
+          # "arn:aws:s3:::terraform-S3bucket-test/*"
+        # ]
+      # }
+    # ]
+  # })
+# }
+resource "aws_iam_role_policy" "github_actions_ec2_policy" {
+  name = "GitHubActionsEC2Policy"
   role = aws_iam_role.github_actions_role.id
 
   policy = jsonencode({
@@ -51,19 +73,54 @@ resource "aws_iam_role_policy" "github_actions_policy" {
       {
         Effect = "Allow",
         Action = [
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject"
+          "ec2:RunInstances",
+          "ec2:DescribeInstances",
+          "ec2:TerminateInstances",
+          "ec2:CreateTags"
         ],
-        Resource = [
-          "arn:aws:s3:::terraform-S3bucket-test",
-          "arn:aws:s3:::terraform-S3bucket-test/*"
-        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = "iam:PassRole",
+        Resource = aws_iam_role.github_actions_role.arn
       }
     ]
   })
 }
 
+# Create a security group for the EC2 instance
+resource "aws_security_group" "instance_sg" {
+  name        = "ec2-instance-sg"
+  description = "Security group for EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH (restrict in production)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (us-east-1, update as needed)
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance_sg.name]
+
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
+}
+
+# Output the IAM role ARN for use in GitHub										  
 # 5. (Optional) Output the IAM role ARN for use in GitHub Actions
 output "role_arn" {
   value = aws_iam_role.github_actions_role.arn
